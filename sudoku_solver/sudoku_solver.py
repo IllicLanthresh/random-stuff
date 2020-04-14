11816ee665882b927a1ad38f228e5a95b9b0deab
Miguel Vallepir <miguel.vallespir@omniaccess.com>
2020-04-14 08:47:28 +0200

    first working version

--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -1,27 +1,45 @@
 import typing
 from itertools import chain
-from .sudoku import Sudoku
+from sudoku import Sudoku, COLUMN_NAMES, ROW_NAMES
 
 
 class SudokuSolver:
     def __init__(self, board: typing.List[typing.List[int]]):
         self.sudoku = Sudoku(board)
 
+    def solve(self):
+        prev_board = None
+        while self.sudoku.board != prev_board:
+            prev_board = self.sudoku.board.copy()
+            self.step()
+
     def step(self):
         for cell in self.sudoku.editable_cells:
-            no_nums = {_ for _ in chain(self.quadrant_nums(cell), self.vertical_nums(cell), self.horizontal_nums(cell))}
-            possible_nums = {_ for _ in range(1, 10)} - no_nums
+            no_go_nums = set()
+            no_go_cells = chain(self.quadrant_cells(cell), self.vertical_cells(cell), self.horizontal_cells(cell))
+            for _ in no_go_cells:
+                if len(self.sudoku.board[_]) == 1:
+                    no_go_nums.add(self.sudoku.board[_][0])
+            possible_nums = {_ for _ in range(1, 10)} - no_go_nums
             self.sudoku[cell] = [*possible_nums]
 
-    def quadrant_nums(self, cell: str) -> typing.Iterator[int]:
-        raise NotImplementedError
+    def quadrant_cells(self, cell: str) -> typing.Iterator[str]:
+        col_group = COLUMN_NAMES.find(cell[0]) * 3 // 9
+        row_group = ROW_NAMES.find(cell[1]) * 3 // 9
 
-    def vertical_nums(self, cell: str) -> typing.Iterator[int]:
+        quadrant_cols = COLUMN_NAMES[col_group*3:col_group*3+3]
+        quadrant_rows = ROW_NAMES[row_group*3:row_group*3+3]
+
+        for other_cell in self.sudoku.filled_cells():
+            if other_cell[0] in quadrant_cols and other_cell[1] in quadrant_rows and cell != other_cell:
+                yield other_cell
+
+    def vertical_cells(self, cell: str) -> typing.Iterator[str]:
         for other_cell in self.sudoku.filled_cells():
             if cell[0] == other_cell[0] and cell != other_cell:
                 yield other_cell
 
-    def horizontal_nums(self, cell: str) -> typing.Iterator[int]:
+    def horizontal_cells(self, cell: str) -> typing.Iterator[str]:
         for other_cell in self.sudoku.filled_cells():
             if cell[1] == other_cell[1] and cell != other_cell:
                 yield other_cell
