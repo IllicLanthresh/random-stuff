274bab6194c17b03a17dffdc601ee501a14835e1
Miquel Vallespir <miguel.vallespir@omniaccess.com>
2020-05-01 12:15:14 +0200

    sdfgdfgdsfg

--- aiom/test2.py
+++ aiom/test2.py
@@ -3,48 +3,78 @@ import asyncio
 import aiomultiprocess
 
 
-aiomultiprocess.set_start_method('spawn')
+class Test:
+    BIG_NUMBER = 100000
+
+    def __init__(self):
+        self.number_list = list(range(2, Test.BIG_NUMBER + 1))
 
 
-def remove_if_prime(num, number_list):
-    for div in range(2, num):
-        if not num % div:
-            number_list.remove(num)
-            break
+class SyncTest(Test):
+    def remove_if_prime(self, num):
+        for div in range(2, num):
+            if not num % div:
+                self.number_list.remove(num)
+                break
+
+    def test(self):
+        start = timer()
+        for num in self.number_list.copy():
+            self.remove_if_prime(num)
+        end = timer()
+        print(end - start)
+        return self.number_list
 
 
-def stress_prime(number_list):
-    start = timer()
-    for num in number_list.copy():
-        remove_if_prime(num, number_list)
-    end = timer()
-    print(end - start)
-    return number_list
+class AsyncTest(Test):
+    def __init__(self):
+        super().__init__()
+        self.loop = asyncio.get_event_loop()
+
+    async def remove_if_prime(self, num):
+        for div in range(2, num):
+            if not num % div:
+                self.number_list.remove(num)
+                break
+
+    async def coro(self):
+        start = timer()
+        tasks = [self.remove_if_prime(num) for num in self.number_list.copy()]
+        await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
+        end = timer()
+        print(end - start)
+        return self.number_list
+
+    def test(self):
+        return self.loop.run_until_complete(self.coro())
 
 
-async def async_stress_prime(number_list):
-    start = timer()
-    tasks = [asyncio.coroutine(remove_if_prime)(num, number_list) for num in number_list.copy()]
-    await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
-    end = timer()
-    print(end - start)
-    return number_list
+class PoolTest(Test):
+    def __init__(self):
+        super().__init__()
+        aiomultiprocess.set_start_method('spawn')
+        self.pool_count = 4
+
+    def remove_if_prime(self, num):
+        for div in range(2, num):
+            if not num % div:
+                self.number_list.remove(num)
+                break
+
+    def chunk_list(self):
+        pass
+
+    def pool_stress_prime(self):
+
+        for _ in range(self.pool_count):
+            pools = []
+            async with aiomultiprocess.Pool() as pool:
+                print(await pool.map(get, urls))
+
+    def test(self):
+        pass
 
 
-# def chunk(l, n):
-#     pass
-
-# def pool_stress_prime(r, pool_count):
-#
-#     for _ in range(pool_count):
-#         pools = []
-#         async with aiomultiprocess.Pool() as pool:
-#             print(await pool.map(get, urls))
-
-
-BIG_NUMBER = 10000
-POOL_COUNT = 4
-
-print(*stress_prime(list(range(2, BIG_NUMBER + 1))), sep=', ')
-print(*asyncio.run(async_stress_prime(list(range(2, BIG_NUMBER + 1)))), sep=', ')
-# print(*pool_stress_prime(BIG_NUMBER, POOL_COUNT), sep=', ')
+print(*SyncTest().test(), sep=', ')
+print(*AsyncTest().test(), sep=', ')
+print(*PoolTest().test(), sep=', ')
